2b580c22a031ffd3c36e8b05774ec1ba3beddcd1
Matheus Araujo <matheusnewff@gmail.com>
2020-06-28 18:27:45 -0300

    Porfolio refatorado com node no back-endd e template engine nunjucks no front-end

---
+++ data.js
@@ -0,0 +1,35 @@
+//Data-base 
+module.exports = [
+        {
+            id: 'ghTrp1x_1As',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 100
+        },
+
+        {
+            id: '2alg7MQ6_sI',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 678
+        },
+        {
+            id: 'vqrjFnq3-uo',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 300
+        },
+        {
+            id: 'GykTLqODQuU',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 700
+        },
+        {
+            id: 'H91DhKPjhPk',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 900
+        },
+        {
+            id: 'hN0zWNuZJC4',
+            description: ' Esta é uma api rest construída usando o que há de melhor            nas tecnologias WEB',
+            value: 100
+        }
+
+]
---
+++ server.js
@@ -0,0 +1,45 @@
+//importando express
+const express  = require('express')
+const nunjucks = require('nunjucks')
+
+const server   = express()
+const products = require('./data')
+
+//Setar o endereço dos arquivos estáticos
+server.use(express.static('public'))
+
+//setar o template engine
+server.set('view engine', 'njk')
+
+//configurar o nunjucks
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false
+})
+
+//Rotas
+server.get('/', (req, res) => {
+    const about = {
+        avatar: 'https://avatars2.githubusercontent.com/u/61169797?s=400&u=b758165f6d23a50900dc000fd126f0e14a1e45ab&v=4',
+        name: 'Matheus Araújo',
+        subtitle: 'Programador iniciante em full-stack',
+        about: 'Atualmente realizando o bootcamp LaunchBase da melhor escola de programção do país  <a href="http://rocketseat.com.br" target="_blank">Rockeatseat</a>',
+        links: [
+           {name: 'Github', url:'https://github.com/MatheusAraujoPro'},
+           {name: 'Linkdin', url: ''}
+            
+        ]
+    }
+
+    return res.render('about', {data: about}) 
+})
+
+server.get('/projetcs', (req, res) => {
+    return res.render('projetcs', {itens: products}) 
+})
+
+//Criando o servidor
+server.listen(8080, () =>{
+    console.log('Running') 
+})
+
