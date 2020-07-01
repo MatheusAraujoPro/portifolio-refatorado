e9c6c804dbede18e40b15a024c3212bd63123d23
Matheus Araujo <matheusnewff@gmail.com>
2020-06-30 23:46:14 -0300

    Pagina de videos funcionando

--- public/scripts.js
+++ public/scripts.js
@@ -4,18 +4,11 @@ const btnClose     = document.querySelector('.close_modal');
 
 for(let card of cards){
     card.addEventListener("click", function(){
-        let video_id = card.getAttribute('id'); 
-        modalOverlay.classList.add('actived');    
-        modalOverlay.querySelector('iframe').src=`http://www.youtube.com/embed/${video_id}`;       
+        let video_id = card.getAttribute('id');
+        window.location.href=`/video?id=${video_id}`
 
     })
 }
 
 
-btnClose.addEventListener('click', function(){
-    modalOverlay.classList.remove('actived');
-    modalOverlay.querySelector('iframe').src= ''
 
-})
-
-console.log(iframe)
